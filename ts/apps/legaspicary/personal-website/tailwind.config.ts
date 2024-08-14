4cfedbef7728cc077d2d9a679627fe8d4e6ed09d
Cary Legaspi <legaspicary@gmail.com>
2024-08-14 23:09:13 +0800

    import base config from de-syde-projx

--- ts/apps/legaspicary/personal-website/tailwind.config.ts
+++ ts/apps/legaspicary/personal-website/tailwind.config.ts
@@ -1,20 +1,24 @@
 import type { Config } from "tailwindcss";
+import defaultConfig from "@de-syd-projx/reactjs/base.tailwind.config";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: defaultConfig.darkMode ?? [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"@de-syd-projx/reactjs",
+	],
+	theme: {
+		extend: {
+			...defaultConfig.theme?.extend,
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [...(defaultConfig.plugins ?? [])],
 };
 export default config;
